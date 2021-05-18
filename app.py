a12d44dda48c109cc43d0043406ac4aeb28d1108
root <root@DESKTOP-ALQM55G.localdomain>
2021-05-18 19:12:45 +0200

    fourth commit

--- app.py
+++ app.py
@@ -19,4 +19,20 @@ with open(a, 'r') as f:
             newdata=re.sub('<head>','<head>' + code, data)
             f.write(newdata)
 
-st.title("Yo tout le monde")
+import streamlit.components.v1 as components
+
+_radio_button = components.declare_component(
+    "radio_button", url="http://localhost:3001",
+)
+
+
+def custom_radio_button(label, options, default, key=None):
+    return _radio_button(label=label, options=options, default=default, key=key)
+
+
+result = custom_radio_button(
+    "How many bats?",
+    options=["one bat", "TWO bats", "THREE bats", "FOUR BATS! ah ah ah!"],
+    default="one bat",
+)
+st.write("This many: %s" % result)
