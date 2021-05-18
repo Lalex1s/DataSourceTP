77d450c236d597f394849b699835bab283921eb5
root <root@DESKTOP-ALQM55G.localdomain>
2021-05-18 19:21:19 +0200

    fourth commit

--- app.py
+++ app.py
@@ -11,28 +11,5 @@ code = """<!-- Global site tag (gtag.js) - Google Analytics -->
   gtag('config', 'UA-197317436-1');
 </script>"""
 
-a=os.path.dirname(st.__file__) + '/static/index.html'
-with open(a, 'r') as f:
-    data=f.read()
-    if len(re.findall('UA-', data)) == 0:
-        with open(a, 'w') as f:
-            newdata=re.sub('<head>','<head>' + code, data)
-            f.write(newdata)
+st.title("This is my site")
 
-import streamlit.components.v1 as components
-
-_radio_button = components.declare_component(
-    "radio_button", url="http://localhost:3001",
-)
-
-
-def custom_radio_button(label, options, default, key=None):
-    return _radio_button(label=label, options=options, default=default, key=key)
-
-
-result = custom_radio_button(
-    "How many bats?",
-    options=["one bat", "TWO bats", "THREE bats", "FOUR BATS! ah ah ah!"],
-    default="one bat",
-)
-st.write("This many: %s" % result)
